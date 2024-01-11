5c3bdf4ce5f97dc034851904bf8e5307bfdb31ac
mhassankhanw3 <mhassankhanw3@gmail.com>
2024-01-11 21:03:56 +0500

    native/push

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text className="bg-red-600">Hello world</Text>
+    </View>
+  );
+}
+
+
+export default App;
